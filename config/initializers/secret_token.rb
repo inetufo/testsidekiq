465eb7e188440c2cbed19af53cc1c7277752543d
inetufo <inetufo@hotmail.com>
2012-09-16 03:49:17 +0800

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testsidekiq::Application.config.secret_token = '4de9ea3e59ba18cf7c1d6c2b1929a0c326d2171e7f4c520bafc59bf4d4e76f22466cde299a2e026c9c179268d3724d95c960358d280dea4577d0f2964e341bd9'
